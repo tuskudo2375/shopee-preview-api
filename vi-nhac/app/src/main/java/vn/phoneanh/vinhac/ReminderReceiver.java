c2eeae390705e217f6efb36ca2d7b8ba9306b225
tuskudo2375 <tuskudo@gmail.com>
2026-08-24 15:43:43 +0700

    Add Vi Nhac MVP: app/src/main/java/vn/phoneanh/vinhac/ReminderReceiver.java

---
+++ vi-nhac/app/src/main/java/vn/phoneanh/vinhac/ReminderReceiver.java
@@ -0,0 +1,9 @@
+package vn.phoneanh.vinhac;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class ReminderReceiver extends BroadcastReceiver {
+    @Override public void onReceive(Context context, Intent intent) { NotificationHelper.channels(context); NotificationHelper.reminder(context); ReminderScheduler.schedule(context); }
+}
