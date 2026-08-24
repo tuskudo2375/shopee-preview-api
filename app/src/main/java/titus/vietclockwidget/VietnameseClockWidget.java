9d7bf8f08d5b72d76318585b69d2c351706b08ff
tuskudo2375 <tuskudo@gmail.com>
2026-08-24 19:40:49 +0700

    Remove month row and format solar date

--- app/src/main/java/titus/vietclockwidget/VietnameseClockWidget.java
+++ app/src/main/java/titus/vietclockwidget/VietnameseClockWidget.java
@@ -135,13 +135,12 @@ public class VietnameseClockWidget extends AppWidgetProvider {
         views.setTextColor(R.id.tv_hour_ones, hour.charAt(1) == '1' ? red : white);
         views.setTextViewText(R.id.tv_minute, minute);
         views.setTextViewText(R.id.tv_weekday, weekday(now.get(Calendar.DAY_OF_WEEK)));
-        views.setTextViewText(R.id.tv_month, "Tháng " + (now.get(Calendar.MONTH) + 1));
         views.setTextViewText(R.id.tv_solar_date, String.format(
                 Locale.US,
-                "%04d-%02d-%02d",
-                now.get(Calendar.YEAR),
+                "%02d/%02d/%04d",
+                now.get(Calendar.DAY_OF_MONTH),
                 now.get(Calendar.MONTH) + 1,
-                now.get(Calendar.DAY_OF_MONTH)
+                now.get(Calendar.YEAR)
         ));
         views.setTextViewText(R.id.tv_lunar_date, LunarCalendar.shortLabel(now));
 
